68095fbe137c4fcce0054bd3c4e94c1ebc465a5d
atomazu <jonaslieken72@gmail.com>
2024-12-10 18:38:03 +0100

    2024-12-10

--- .config/hypr/scripts/lock.sh
+++ .config/hypr/scripts/lock.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-# Define paths for the screenshots
-acershot="/tmp/lock-acer.png"
-lgshot="/tmp/lock-lg.png"
-lenovoshot="/tmp/lock-lenovo.png"
-
-# Take screenshots for each monitor
-grim -o HDMI-A-1 "$acershot"      # Acer XV272U
-grim -o DP-3 "$lgshot"            # LG UltraGear
-grim -o DP-2 "$lenovoshot"        # Lenovo D32q-20B
-
 uwsm app -- hyprlock
